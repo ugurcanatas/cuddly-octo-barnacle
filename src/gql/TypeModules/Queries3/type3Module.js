0086cc499d5103305fab800990acf22f5b8a44b2
ugurcanatas <ugurcanemre93@gmail.com>
2021-04-03 11:59:05 +0300

    Cleanup

--- src/gql/TypeModules/Queries3/type3Module.js
+++ src/gql/TypeModules/Queries3/type3Module.js
@@ -6,25 +6,8 @@ export const type3Module = createModule({
   dirname: __dirname,
   typeDefs: gql`
     type TripTypeMinMaxLookup {
-      #   id: ID
-      #   VendorID: Int
-      #   tpep_pickup_datetime: Date
-      #   tpep_dropoff_datetime: Date
       passenger_count: Int
-      #   trip_distance: DoubleType
-      #   RatecodeID: Int
-      #   store_and_fwd_flag: String
       PULocationID: Int
-      #   DOLocationID: Int
-      #   payment_type: Int
-      #   fare_amount: DoubleType
-      #   extra: DoubleType
-      #   mta_tax: DoubleType
-      #   tip_amount: DoubleType
-      #   tolls_amount: DoubleType
-      #   improvement_surcharge: DoubleType
-      #   total_amount: DoubleType
-      #   congestion_surcharge: DoubleType
       lookup_result: [TaxiZone]
       min: DoubleType
       max: DoubleType
